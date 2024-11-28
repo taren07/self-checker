45ddc797992d7579d945d0ca13471c2c7c385b53
taren07 <taro.enchi@gmail.com>
2024-11-28 23:55:33 +0900

    create digitSpan

---
+++ src/routes/numbers/digitSpan.tsx
@@ -0,0 +1,5 @@
+import { component$ } from "@builder.io/qwik";
+
+export const DigitSpan = component$(() => {
+	return <></>;
+});
---
+++ src/routes/numbers/index.tsx
@@ -0,0 +1,6 @@
+import { component$ } from "@builder.io/qwik";
+import { DigitSpan } from "./digitSpan";
+
+export const DigitSpanBackward = component$(() => {
+	return <DigitSpan />;
+});
