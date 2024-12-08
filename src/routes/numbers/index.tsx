0364f8ba6d76043c92fc8896f90ff4353dd8d282
taren07 <taro.enchi@gmail.com>
2024-12-08 23:22:48 +0900

    create test step

--- src/routes/numbers/index.tsx
+++ src/routes/numbers/index.tsx
@@ -1,6 +1,50 @@
-import { component$ } from "@builder.io/qwik";
-import { DigitSpan } from "./digitSpan";
+import {
+	component$,
+	useStore,
+	createContextId,
+	useContextProvider,
+	useContext,
+} from "@builder.io/qwik";
+import { numbersContextId } from "~/context/id";
+import { Top } from "./top";
+
+export type NumbersStore = {
+	step:
+		| { tag: "Top" }
+		| { tag: "CountDown" }
+		| { tag: "CountDownFinished" }
+		| { tag: "ShowDigit" }
+		| { tag: "Answer" }
+		| { tag: "Result" };
+	direction: String;
+	answerLength: Number;
+	answers: Array<Number>;
+};
+
+export const NumbersContext = createContextId<NumbersStore>(numbersContextId);
 
 export const DigitSpanBackward = component$(() => {
-	return <DigitSpan />;
+	const numbersContext = useContext(NumbersContext);
+	const numbersStore = useStore<NumbersStore>({
+		step: { tag: "Top" },
+		direction: "backward",
+		answerLength: 3,
+		answers: [],
+	});
+	useContextProvider(NumbersContext, numbersStore);
+
+	switch (numbersStore.step.tag) {
+		case "Top":
+			return <Top />;
+		case "CountDown":
+			return <></>;
+		case "ShowDigit":
+			return <></>;
+		case "Answer":
+			return <></>;
+		case "Result":
+			return <></>;
+	}
+
+	return <></>;
 });
--- src/routes/numbers/top.tsx
+++ src/routes/numbers/top.tsx
@@ -1,11 +1,24 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useContext, useStore } from "@builder.io/qwik";
+import { NumbersContext, NumbersStore } from ".";
+
+export type PartialNumbersStore = Omit<NumbersStore, "direction" | "answers"> &
+	Partial<Pick<NumbersStore, "direction" | "answers">>;
 
 export const Top = component$(() => {
+	const numbersContext = useContext(NumbersContext);
+	const start = useStore<PartialNumbersStore>(numbersContext);
+
+	const startHandler = () => {
+		start.step = { tag: "StartTrial" };
+		start.answerLength = 3;
+	};
+
 	return (
 		<main>
 			<h1>記憶力のチェック</h1>
 			<p>表示される数字を覚えて</p>
 			<p>表示された順番とは逆の順番で回答してください</p>
+			<button onClick$={startHandler}>スタート</button>
 		</main>
 	);
 });
